b693f57c170a2d378c2e314ff0d00a023185d670
Md.Afizul Islam <afizul2459@gmail\.com>
2024-10-20 22:29:18 +0600

    initial commit

--- obj/Debug/net8.0/Ecommerce-WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce-WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce-WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc47b61c9fb1bb379204405e31cf98ba0e68526")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce-WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce-WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
