217914ad3ecae94017bc17f9de2651ad4b8f2f43
Md.Afizul Islam <afizul2459@gmail\.com>
2024-10-21 22:27:50 +0600

    initial commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+//Rest API => GET, POST, PUT, DELETE
+
+app.MapGet("/hello",()=>
+{
+    return"Get Method: Hello World";
+});
 
 
 app.Run();
